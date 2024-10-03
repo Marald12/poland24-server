de62ac752078a121149c7edf849c65b9a95a00d3
Zmey <catiklou@gmail.com>
2024-10-03 13:33:19 +0300

    init and init database

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { mongoConfig } from './config/mongo.config'
+
+@Module({
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath: '.env',
+			isGlobal: true
+		}),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: mongoConfig
+		})
+	],
+	controllers: [],
+	providers: []
+})
+export class AppModule {}
